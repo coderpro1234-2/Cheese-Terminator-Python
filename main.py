ab499793e5777f2264331db3301fcafde34c430b
SirCodesALot <omaraustin1231@gmail.com>
2024-12-22 16:34:52 +1100

    Rewrote my terrible 3 year old code into some better code.

--- main.py
+++ main.py
@@ -1,55 +1,44 @@
 import pygame
-#Import all of the level data
-from gamedata import *
-#Start
+from levelData import levels
+from collections import Counter
+
 pygame.init()
-#Position/Game variables
-DEBUG = False
-WIDTH = 400
-HEIGHT = 400
-cheeselist = []
-endlist = []
-mousepos = 0
-mousedir = 3
-levelwidth = 0
-levelheight = 0
-mouseOffsets = [-levelwidth,levelwidth,-1,1]
-blocksize = 30
-gamelist = []
-level = 1
-maxLevel = 50
-emptyblock = [1,2,3,4,5]
-paused = True
-start = True
-reset = False
-end = False
-#Colo(u)r variables
-yellow = (255,255,0)
-black = (0,0,0)
-floor = (192,192,192)
-wall1 = (255,255,255)
-wall2 = (128,128,128)
-#Loop variables
-running = True
-#Screen variables
-screen = pygame.display.set_mode((WIDTH,HEIGHT))
-mouseBase = pygame.image.load("assets/images/mouse.png")
-mouseUp = mouseBase.subsurface((0,270,30,30))
-mouseDown = mouseBase.subsurface((0,90,30,30))
-mouseLeft = mouseBase.subsurface((0,180,30,30))
-mouseRight = mouseBase.subsurface((0,0,30,30))
-cheeseBase = pygame.image.load("assets/images/cheese.png")
-cheeseWhole = cheeseBase.subsurface((0,30,30,30))
-cheeseSlice = cheeseBase.subsurface((0,0,30,30))
-targetBase = pygame.image.load("assets/images/goal.png")
-targetRed = targetBase.subsurface((0,0,30,30))
-targetBlue = targetBase.subsurface((0,30,30,30))
-icon = pygame.image.load("assets/icon.png")
-#Screen display
-pygame.display.set_caption("Cheese Terminator")
-pygame.display.set_icon(icon)
-pygame.display.update()
-#Classes
+
+class Mouse():
+  def __init__(self,x,y):
+    self.x = x
+    self.y = y
+    self.direction = 3
+  def addPos(self,a,b):
+    if len(a) != len(b):
+      raise ValueError('Both lists must be the same length.')
+    return tuple(a[i]+b[i] for i in range(len(a)))
+  def checkPos(self,pos):
+    if pos[0] < 0 or pos[0] >= levels[currentLevel].width:
+      return False
+    if pos[1] < 0 or pos[1] >= levels[currentLevel].height:
+      return False
+    if levels[currentLevel].data[pos[1]][pos[0]] == 0:
+      return False
+    return True
+  def move(self,direction):
+    global cheese
+    self.direction = direction
+    do = directionOffsets[direction]
+    newPos = self.addPos([self.x,self.y],do)
+    if not self.checkPos(newPos):
+      return
+    if newPos in cheese:
+      newnewPos = self.addPos(newPos,do)
+      if not self.checkPos(newnewPos) or newnewPos in cheese:
+        return
+      cheese[cheese.index(newPos)] = newnewPos
+    [self.x,self.y] = newPos
+  def setPos(self,pos):
+    self.x = pos[0]
+    self.y = pos[1]
+    self.direction = 3
+
 class Text():
   def __init__(self, text, pos, size, color):
     self.text = text
@@ -62,238 +51,160 @@ class Text():
     self.rect.center = self.pos
   def draw(self):
     screen.blit(self.render, self.rect)
-#Text
-start1 = Text("Cheese Terminator", (WIDTH // 2, ((HEIGHT // 2)-30)), 32, yellow)
-start2 = Text("Press Enter To Start", (WIDTH // 2, ((HEIGHT // 2)-3)), 20, yellow)
-start3 = Text("Use Arrow Keys To Move", (WIDTH // 2, ((HEIGHT // 2)+18)), 20, yellow)
-next1 = Text("Well Done!", (WIDTH // 2, ((HEIGHT // 2)-15)), 32, yellow)
-next2 = Text("Press Enter To Continue", (WIDTH // 2, ((HEIGHT // 2)+15)), 20, yellow)
-win1 = Text("You Won!!! :D", (WIDTH // 2, ((HEIGHT // 2)-15)), 32, yellow)
-win2 = Text("Press Enter To Play Again", (WIDTH // 2, ((HEIGHT // 2)+15)), 20, yellow)
-reset1 = Text("Restart The Level?", (WIDTH // 2, ((HEIGHT // 2)-30)), 32, yellow)
-reset2 = Text("Press Enter To Restart", (WIDTH // 2, ((HEIGHT // 2)-3)), 20, yellow)
-reset3 = Text("Press Any Arrow Key To Cancel", (WIDTH // 2, ((HEIGHT // 2)+18)), 20, yellow)
-#Functions
-def updateLevel(lvl):
-  global endlist, cheeselist, mousepos, screen, levelwidth, levelheight, gamelist, mousedir, mouseOffsets
-  pygame.display.set_caption("Cheese Terminator - Level "+str(ldv.index(lvl)+1))
-  level = lvl[1:]
-  endlist = []
-  cheeselist = []
-  drawid = 0
-  mousedir = 3
-  width = lvl[0]
-  height = len(level) / width
-  if height % 1 != 0:
-    raise ValueError("Invalid level data")
-  else:
-    height = int(height)
-  if DEBUG:
-    print(f'Level: {str(ldv.index(lvl)+1)}, Width: {width}, Height: {height}')
-  screen = pygame.display.set_mode((round(width*blocksize),round(height*blocksize)))
-  levelwidth = width
-  levelheight = height
-  mouseOffsets = [-levelwidth,levelwidth,-1,1]
-  for _ in range(levelheight):
-    for _ in range(levelwidth):
-      if level[drawid] == 2:
-        endlist.append(drawid)
-      elif level[drawid] == 3:
-        cheeselist.append(drawid)
-      elif level[drawid] == 4:
-        mousepos = drawid
-      elif level[drawid] == 5:
-        cheeselist.append(drawid)
-        endlist.append(drawid)
-      drawid = drawid + 1
-  gamelist = level
-def drawScreen():
-  if paused:
+
+screen = pygame.display.set_mode((400, 400))
+icon = pygame.image.load("assets/icon.png")
+pygame.display.set_caption("Cheese Terminator")
+pygame.display.set_icon(icon)
+
+currentLevel = 0
+endLevel = 4
+paused = 1
+mouse = Mouse(0,0)
+cheese = []
+goals = []
+directionOffsets = [[0,-1],[0,1],[-1,0],[1,0]]
+movementKeys = [pygame.K_UP,pygame.K_DOWN,pygame.K_LEFT,pygame.K_RIGHT]
+blockSize = 30
+
+textColour = (255,255,0)
+black = (0,0,0)
+floor = (192,192,192)
+wallA = (255,255,255)
+wallB = (128,128,128)
+
+mouseBase = pygame.image.load("assets/images/mouse.png")
+mouseUp = mouseBase.subsurface((0,270,30,30))
+mouseDown = mouseBase.subsurface((0,90,30,30))
+mouseLeft = mouseBase.subsurface((0,180,30,30))
+mouseRight = mouseBase.subsurface((0,0,30,30))
+cheeseBase = pygame.image.load("assets/images/cheese.png")
+cheeseWhole = cheeseBase.subsurface((0,30,30,30))
+cheeseSlice = cheeseBase.subsurface((0,0,30,30))
+targetBase = pygame.image.load("assets/images/goal.png")
+targetRed = targetBase.subsurface((0,0,30,30))
+targetBlue = targetBase.subsurface((0,30,30,30))
+
+start1 = Text("Cheese Terminator", (200, (200-30)), 32, textColour)
+start2 = Text("Press Enter To Start", (200, (200-3)), 20, textColour)
+start3 = Text("Use Arrow Keys To Move", (200, (200+18)), 20, textColour)
+next1 = Text("Well Done!", (200, (200-15)), 32, textColour)
+next2 = Text("Press Enter To Continue", (200, (200+15)), 20, textColour)
+win1 = Text("You Won!!! :D", (200, (200-15)), 32, textColour)
+win2 = Text("Press Enter To Play Again", (200, (200+15)), 20, textColour)
+reset1 = Text("Restart The Level?", (200, (200-30)), 32, textColour)
+reset2 = Text("Press Enter To Restart", (200, (200-3)), 20, textColour)
+reset3 = Text("Press Any Arrow Key To Cancel", (200, (200+18)), 20, textColour)
+
+def setScreenSize(w,h):
+  global screen
+  screen = pygame.display.set_mode((w,h))
+
+def updateLevel():
+  global cheese, goals
+  level = levels[currentLevel]
+  pygame.display.set_caption("Cheese Terminator - Level "+str(currentLevel+1))
+  setScreenSize(level.width*blockSize,level.height*blockSize)
+  cheese = level.cheese.copy()
+  goals = level.goals.copy()
+  mouse.setPos(level.mouse)
+
+def checkWon():
+  return Counter(tuple(cheese)) == Counter(goals)
+
+def drawLevel():
+  if paused > 0:
     return
-  drawid = 0
-  for drawy in range(levelheight):
-    drawx = 0
-    for drawx in range(levelwidth):
-      if gamelist[drawid] == 0:
-        pygame.draw.rect(screen,floor,(drawx*blocksize,drawy*blocksize,blocksize,blocksize))
-        for offset in mouseOffsets:
-          if isLegalOffset(drawid,offset):
-            if offset == -levelwidth:
-              pygame.draw.line(screen,wall1,(drawx*blocksize,drawy*blocksize),(drawx*blocksize+blocksize,drawy*blocksize),2)
-            elif offset == levelwidth:
-              pygame.draw.line(screen,wall2,(drawx*blocksize,drawy*blocksize+blocksize-2),(drawx*blocksize+blocksize,drawy*blocksize+blocksize-2),2)
-            elif offset == -1:
-              pygame.draw.line(screen,wall1,(drawx*blocksize,drawy*blocksize),(drawx*blocksize,drawy*blocksize+blocksize),2)
+  level = levels[currentLevel]
+  screen.fill(floor)
+  for y in range(level.height):
+    for x in range(level.width):
+      if level.data[y][x] == 0:
+        for offset in directionOffsets:
+          if mouse.checkPos(mouse.addPos([x,y],offset)):
+            if offset == directionOffsets[0]:
+              pygame.draw.line(screen,wallA,(x*blockSize,y*blockSize),(x*blockSize+blockSize,y*blockSize),2)
+            elif offset == directionOffsets[1]:
+              pygame.draw.line(screen,wallB,(x*blockSize,y*blockSize+blockSize-2),(x*blockSize+blockSize,y*blockSize+blockSize-2),2)
+            elif offset == directionOffsets[2]:
+              pygame.draw.line(screen,wallA,(x*blockSize,y*blockSize),(x*blockSize,y*blockSize+blockSize),2)
             else:
-              pygame.draw.line(screen,wall2,(drawx*blocksize+blocksize-2,drawy*blocksize),(drawx*blocksize+blocksize-2,drawy*blocksize+blocksize),2)
+              pygame.draw.line(screen,wallB,(x*blockSize+blockSize-2,y*blockSize),(x*blockSize+blockSize-2,y*blockSize+blockSize),2)
       else:
-        pygame.draw.rect(screen,floor,(drawx*blocksize,drawy*blocksize,blocksize,blocksize))
-      if drawid in cheeselist:
-        if drawid in endlist:
-          screen.blit(cheeseSlice,(drawx*blocksize,drawy*blocksize))
-        else:
-          screen.blit(cheeseWhole,(drawx*blocksize,drawy*blocksize))
-      elif drawid in endlist:
-        screen.blit(targetRed,(drawx*blocksize,drawy*blocksize))
-      if mousepos == drawid:
+        pygame.draw.rect(screen,floor,(x*blockSize,y*blockSize,blockSize,blockSize))
+      cheeseGoal = 0
+      if (x,y) in cheese:
+        cheeseGoal += 1
+      if (x,y) in goals:
+        cheeseGoal += 2
+      
+      if cheeseGoal == 1:
+        screen.blit(cheeseWhole,(x*blockSize,y*blockSize))
+      elif cheeseGoal == 2:
+        screen.blit(targetRed,(x*blockSize,y*blockSize))
+      elif cheeseGoal == 3:
+        screen.blit(cheeseSlice,(x*blockSize,y*blockSize))
+      
+      if (x,y) == (mouse.x,mouse.y):
+        mousedir = mouse.direction
         if mousedir == 0:
-          screen.blit(mouseUp,(drawx*blocksize,drawy*blocksize))
+          screen.blit(mouseUp,(x*blockSize,y*blockSize))
         elif mousedir == 1:
-          screen.blit(mouseDown,(drawx*blocksize,drawy*blocksize))
+          screen.blit(mouseDown,(x*blockSize,y*blockSize))
         elif mousedir == 2:
-          screen.blit(mouseLeft,(drawx*blocksize,drawy*blocksize))
+          screen.blit(mouseLeft,(x*blockSize,y*blockSize))
         else:
-          screen.blit(mouseRight,(drawx*blocksize,drawy*blocksize))
-      drawid = drawid + 1
-def drawPause(nextlevel=False):
-  if not paused:
+          screen.blit(mouseRight,(x*blockSize,y*blockSize))
+
+def drawPause():
+  if paused == 0:
     return
   screen = pygame.display.set_mode((400,400))
   screen.fill(black)
-  if nextlevel:
-    next1.draw()
-    next2.draw()
-  elif start:
+  if paused == 1:
     start1.draw()
     start2.draw()
     start3.draw()
-  elif reset:
+  elif paused == 2:
     reset1.draw()
     reset2.draw()
     reset3.draw()
-  elif end:
+  elif paused == 3:
+    next1.draw()
+    next2.draw()
+  elif paused == 4:
     win1.draw()
     win2.draw()
-def isLegalOffset(pos,off,checkWall=True):
-  newpos = pos+off
-  if abs(off) == levelwidth:
-    if newpos < 0 or newpos >= len(gamelist):
-      return False
-  elif off == -1:
-    if newpos+1 % levelwidth == 0:
-      return False
-  elif off == 1:
-    if newpos % levelwidth == 0:
-      return False
-  if gamelist[newpos] == 0 and checkWall:
-    return False
-  return True
-def checkBlock(pos,dir):
-  if not isLegalOffset(pos,mouseOffsets[dir]):
-    return 0
-  if pos+mouseOffsets[dir] in cheeselist:
-    if isLegalOffset(pos,mouseOffsets[dir]*2):
-      if pos+(mouseOffsets[dir]*2) in cheeselist or gamelist[pos+(mouseOffsets[dir]*2)] == 0:
-        return 0
-      return 2
-    return 0
-  return 1
-def checkWon():
-  q = 0
-  a = 0
-  while q < len(endlist):
-    if endlist[q] in cheeselist:
-      a = a + 1
-    q = q + 1
-  return a == q
-#Main loop
+
 drawPause()
-while running:
+pygame.display.update()
+while True:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
-      running = False
+      exit()
     if event.type == pygame.KEYDOWN:
-      if event.key == pygame.K_UP:
-        if start or end:
-          pass
-        elif reset:
-          reset = False
-          paused = False
-          screen = pygame.display.set_mode((levelwidth*blocksize,levelheight*blocksize))
+      if paused in [0,2] and event.key in movementKeys:
+        if paused == 2:
+          paused = 0
+          setScreenSize(levels[currentLevel].width,levels[currentLevel].height)
         else:
-          mousedir = 0
-          offset = mouseOffsets[mousedir]
-          blocked = checkBlock(mousepos,mousedir)
-          if blocked > 0:
-            mousepos = mousepos+offset
-            if blocked == 2:
-              cheeselist[cheeselist.index(mousepos)] += offset
-      if event.key == pygame.K_DOWN:
-        if start or end:
-          pass
-        elif reset:
-          reset = False
-          paused = False
-          screen = pygame.display.set_mode((levelwidth*blocksize,levelheight*blocksize))
-        else:
-          mousedir = 1
-          offset = mouseOffsets[mousedir]
-          blocked = checkBlock(mousepos,mousedir)
-          if blocked > 0:
-            mousepos = mousepos+offset
-            if blocked == 2:
-              cheeselist[cheeselist.index(mousepos)] += offset
-      if event.key == pygame.K_LEFT:
-        if start or end:
-          pass
-        elif reset:
-          reset = False
-          paused = False
-          screen = pygame.display.set_mode((levelwidth*blocksize,levelheight*blocksize))
-        else:
-          mousedir = 2
-          offset = mouseOffsets[mousedir]
-          blocked = checkBlock(mousepos,mousedir)
-          if blocked > 0:
-            mousepos = mousepos+offset
-            if blocked == 2:
-              cheeselist[cheeselist.index(mousepos)] += offset
-      if event.key == pygame.K_RIGHT:
-        if start or end:
-          pass
-        elif reset:
-          reset = False
-          paused = False
-          screen = pygame.display.set_mode((levelwidth*blocksize,levelheight*blocksize))
-        else:
-          mousedir = 3
-          offset = mouseOffsets[mousedir]
-          blocked = checkBlock(mousepos,mousedir)
-          if blocked > 0:
-            mousepos = mousepos+offset
-            if blocked == 2:
-              cheeselist[cheeselist.index(mousepos)] += offset
+          mouse.move(movementKeys.index(event.key))
       if event.key == pygame.K_RETURN:
-        if paused:
-          if start:
-            paused = False
-            start = False
-            updateLevel(lvldict[level])
-          if reset:
-            reset = False
-            paused = False
-            updateLevel(lvldict[level])
-          if end:
-            running = False
+        if paused > 0:
+          if paused != 4:
+            paused = 0
+            updateLevel()
+          else:
             exit()
         else:
-          reset = True
-          paused = True
+          paused = 2
           drawPause()
-          
-      if event.key == pygame.K_SPACE and (not paused) and DEBUG:
-        level += 1
-        updateLevel(lvldict[level])
-      if checkWon() and (not paused):
-        if level == maxLevel:
-          paused = True
-          end = True
-          drawPause()
-        else:
-          paused = True
-          reset = True
-          level += 1
-          drawPause(True)
-    WIDTH,HEIGHT = screen.get_size()
-    drawScreen()
+    if checkWon() and paused == 0:
+      currentLevel += 1
+      if currentLevel == endLevel:
+        paused = 4
+      else:
+        paused = 3
+      drawPause()
+    drawLevel()
     pygame.display.update()
--- readme.md
+++ readme.md
@@ -1,13 +1,13 @@
 # Microsoft Cheese Terminator
 
-This Is A Recreation On The Original Game _**Microsoft Cheese Terminator**_
+This is a recreation of the game _**Microsoft Cheese Terminator**_.
 
 _**Microsoft Cheese Terminator**_ is a [Sokoban](http://en.wikipedia.org/wiki/Sokoban "wikipedia:Sokoban") clone developed by Zsolt Krajcsik and published by [Microsoft](https://microsoft.fandom.com/wiki/Microsoft "Microsoft") in 1992 for [Windows 3.x](https://microsoft.fandom.com/wiki/Windows_3.x "Windows 3.x"). It was offered as part of a promotion by Microsoft in Eastern Europe where users would receive a floppy disk containing the game after filling out and mailing a survey.
 
-Taken From [The Microsoft Fandom](https://microsoft.fandom.com/wiki/Microsoft_Cheese_Terminator "Microsoft Fandom")
+Taken from [The Microsoft Fandom](https://microsoft.fandom.com/wiki/Microsoft_Cheese_Terminator "The Microsoft Fandom").
 
 ## How Do I Play?
-Download The [Zip File](https://github.com/coderpro1234-2/Cheese-Terminator-Python/releases/download/v1.0.0/Cheese-Terminator.zip "Download Link") And Run The `main.py` File With `python3 main.py` In A Command Window
+Download and extract the cheeseTerminator.zip file from [here](https://github.com/coderpro1234-2/Cheese-Terminator-Python/releases/latest "Download Link"), and run the `main.py` file with `python3 main.py` in a command prompt.
 
 ## Why Did You Take Over 3 Years To Finish This Game?
 ¯\\\_(ツ)\_/¯
